db8b3fa42e838046656834a835f731fe5a52e443
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2025-01-16 11:30:14 -0800

    See WORK_LOG.md

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Alexandre Haddad-Delaveau
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date   |    Time     |                                                                                                                                                                                                                                                                                                              Update |
+|:-------|:-----------:|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Jan 15 | 21:30-22:00 | Really confused by this problem and how to do it efficiently, so I started by modeling it on paper. I think that going through each element of the list, and updating all previous (lower) temps is probably the best bet. This way there is never an issue where I accidentally pickup a high value and get stuck. |
+| Jan 16 | 11:00-11:30 |                                                                                                                                                                                                                                     Algorithm seems to work. I don't think its super efficient, but it does work... |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,22 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Alexandre Haddad-Delaveau
  */
 
 public class WeatherPatterns {
 
-
     /**
      * Longest Warming Trend
-     * @param temperatures
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
+        // Quick exit conditions
+        if (temperatures == null || temperatures.length == 0) {
+            return 0;
+        }
 
-        return 0;
+        // Keep track of length of runs
+        int[] lengths = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            lengths[i] = 1;
+        }
+
+        int longestRun = 1;
+
+        // Loop through all temps (checking for an increasing temp)
+        for (int i = 1; i < temperatures.length; i++) {
+            // Find which (previous) temps the current temp is greater than
+            for (int j = 0; j < i; j++) {
+                if (temperatures[i] > temperatures[j]) {
+                    // Find the node the colder node with the longest run, and add 1 to the new higher temperature
+                    if (lengths[i] < lengths[j] + 1) {
+                        lengths[i] = lengths[j] + 1;
+                    }
+                }
+            }
+
+            // Update the longest run if any improvements have been made
+            if (lengths[i] > longestRun) {
+                longestRun = lengths[i];
+            }
+        }
+
+        // Return the longest run
+        return longestRun;
     }
 }
